530461f32fec4509d44acf79cd36142a29d277c6
Nate Metzger <metzgenl@mail.uc.edu>
2025-03-12 20:57:44 -0400

    Added Comments

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         //Task 1
         Scanner in = new Scanner(System.in);
         Random rnd = new Random();
